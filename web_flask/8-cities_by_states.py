0ae14e691bd4314d6eff65f0932bfb4e5b3213d9
Olisajioke <chijioke914@gmail.com>
2024-01-01 16:10:56 -0800

    updating task 9

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application.
-"""
+"""python script that starts a Flask web application"""
 
 from flask import Flask, render_template
+from models import *
 from models import storage
-
 app = Flask(__name__)
-
-
-@app.teardown_appcontext
-def teardown_appcontext(exception):
-    """Closes the SQLAlchemy session after each request."""
-    storage.close()
 
 
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
-    """Displays a list of states with their associated cities in HTML."""
-    states = storage.all("State")
-    sorted_states = sorted(states.values(), key=lambda x: x.name)
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
+    return render_template('8-cities_by_states.html', states=states)
 
-    return render_template('8-cities_by_states.html', sorted_states=sorted_states)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
